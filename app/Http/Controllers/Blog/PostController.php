6fc114c3992e76722cb3101d95c58fdaf3bb0dff
Vlada-onf <vladyslava.onufriienko@oa.edu.ua>
2025-06-05 20:47:22 +0300

    Лабораторна робота 3: REST контролери, ієрархія контролерів блогу, Blade шаблони, SoftDeletes

---
+++ app/Http/Controllers/Blog/PostController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Blog;
+
+use App\Models\BlogPost;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // Отримуємо всі записи з таблиці blog_posts
+        $items = BlogPost::all();
+
+        // Повертаємо blade-шаблон 'blog.posts.index' і передаємо йому дані
+        return view('blog.posts.index', compact('items'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory; // Додати цей рядок
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BlogPost extends Model
 {
-    use HasFactory; // Додати цей рядок
+    use HasFactory;
+    use SoftDeletes;
 }
